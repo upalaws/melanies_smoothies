4fbec4b92d7b7238d5fd34972117f38f01a888ad
upalaws <166999976+upalaws@users.noreply.github.com>
2024-04-15 18:06:45 +0530

    Update streamlit_app.py
    Added the functionality of SEARCH_ON field .

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ option = st.selectbox(
 st.write('Your favourite fruit is :', option)
 cnx = st.connection("snowflake") 
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
-##st.dataframe(data = my_dataframe,use_container_width =  True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("SEARCH_ON),col("FRUIT_NAME")).filter(col("SEARCH_ON).isNotNull())
+st.dataframe(data = my_dataframe,use_container_width =  True)
 ingredients_list = st.multiselect('Choose upto 5 ingredients',
                                   my_dataframe,
                                   max_selections = 5)
